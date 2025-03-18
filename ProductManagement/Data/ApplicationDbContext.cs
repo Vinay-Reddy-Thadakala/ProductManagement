2b761500cacdf4ab599e207283ff08f5886ff95f
Vinay-Reddy-Thadakala <vinayreddythadakala@gmail.com>
2025-03-19 01:11:14 +0530

    first commit

---
+++ ProductManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Models;
+
+namespace ProductManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductManagement.Models.Product> Product { get; set; } = default!;
+    }
+}
